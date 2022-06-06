d3d5d69feea98ba37feb47f87b08627b3b0e3409
Ninaad <ninaadps@gmail.com>
2022-06-06 20:46:06 +0530

    complete

--- src/components/Layout/Header.js
+++ src/components/Layout/Header.js
@@ -1,21 +1,21 @@
-import React from "react";
-import classes from './Header.module.css'
-import mealsimage from '../../assets/meals.jpg'
-import HeaderCartButton from './HeaderCartButton';
+import { Fragment } from 'react';
 
-const Header = (props) =>
-{
+import HeaderCartButton from './HeaderCartButton';
+import mealsImage from '../../assets/meals.jpg';
+import classes from './Header.module.css';
+
+const Header = (props) => {
   return (
-   <React.Fragment>
+    <Fragment>
       <header className={classes.header}>
-          <h1>ReactMeals</h1>
-          <HeaderCartButton onClick={props.onShowCart}/>
+        <h1>ReactMeals</h1>
+        <HeaderCartButton onClick={props.onShowCart} />
       </header>
       <div className={classes['main-image']}>
-          <img src={mealsimage} alt="Delicious fooooood!" />
+        <img src={mealsImage} alt='A table full of delicious food!' />
       </div>
-  </React.Fragment>
+    </Fragment>
   );
-}
+};
 
 export default Header;
--- src/components/Meals/AvailableMeals.js
+++ src/components/Meals/AvailableMeals.js
@@ -1,47 +1,52 @@
-import classes from './AvailableMeals.module.css'
 import Card from '../UI/Card';
 import MealItem from './MealItem/MealItem';
+import classes from './AvailableMeals.module.css';
+
 const DUMMY_MEALS = [
-    {
-      id: 'm1',
-      name: 'Paneer Tikka',
-      description: 'Finest paneer and veggies',
-      price: 199,
-    },
-    {
-      id: 'm2',
-      name: 'Chicken Biriyani',
-      description: 'A Hyderabadi specialty!',
-      price: 359,
-    },
-    {
-      id: 'm3',
-      name: 'Aloo tikki Burger',
-      description: 'Blend of Indian Spices',
-      price: 49,
-    },
-    {
-      id: 'm4',
-      name: 'McVeggie Pizza',
-      description: 'Healthy...and green...',
-      price: 699,
-    },
-  ];
-const AvailableMeals = () => 
-{  const Mealslist=DUMMY_MEALS.map( (meal) => <MealItem key={meal.id}
-    name={meal.name}
-    description={meal.description}
-    price={meal.price}
-/> );
+  {
+    id: 'm1',
+    name: 'Sushi',
+    description: 'Finest fish and veggies',
+    price: 22.99,
+  },
+  {
+    id: 'm2',
+    name: 'Schnitzel',
+    description: 'A german specialty!',
+    price: 16.5,
+  },
+  {
+    id: 'm3',
+    name: 'Barbecue Burger',
+    description: 'American, raw, meaty',
+    price: 12.99,
+  },
+  {
+    id: 'm4',
+    name: 'Green Bowl',
+    description: 'Healthy...and green...',
+    price: 18.99,
+  },
+];
+
+const AvailableMeals = () => {
+  const mealsList = DUMMY_MEALS.map((meal) => (
+    <MealItem
+      key={meal.id}
+      id={meal.id}
+      name={meal.name}
+      description={meal.description}
+      price={meal.price}
+    />
+  ));
+
   return (
-  <section className={classes.meals}>
-  <Card>
-      <ul>
-          {Mealslist}
-      </ul>
+    <section className={classes.meals}>
+      <Card>
+        <ul>{mealsList}</ul>
       </Card>
-  </section>
-  )
-}
+    </section>
+  );
+};
 
 export default AvailableMeals;
